b7cd8eea76910d1b7c69c85abe2c6c2bc6b27df0
Eduard David <eduard.david0129@gmail.com>
2024-02-14 02:05:48 +0200

    feat (WIP) added text animation

--- src/app/wip/components/wip-page/wip-page.component.ts
+++ src/app/wip/components/wip-page/wip-page.component.ts
@@ -7,7 +7,13 @@ import { Component } from '@angular/core';
 			<div
 				class="relative top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 items-center justify-center flex"
 			>
-				<p>I am currently working on this site, please come back later!</p>
+				<div class="w-[360px] h-10 flex flex-col justify-center items-center">
+					<p
+						class="text-center flex flex-col animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black text-[18px] sm:text-[13px]"
+					>
+						I am currently working on this site, please come back later!
+					</p>
+				</div>
 			</div>
 		</div>
 	`,
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,44 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
     extend: {
+      screens: {
+        'sm': '350px',
+
+        'md': '960px',
+
+        'lg': '1440px',
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "50%": {
+            width: "100%",
+            visibility: "visible"
+          },
+          "75%": {
+            width: "100%",
+            visibility: "visible"
+          },
+          "100%": {
+            width: "100%",
+            visibility: "visible"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }
+        }
+      },
+      animation: {
+        typing: "typing 6s steps(100, end) infinite alternate, blink .7s step-end infinite "
+      },
       fontFamily: {
         telegraf: ["PPTelegraf", "sans-serif"]
       }
